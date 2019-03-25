470d3fd13706b4794d5301f823e123f343ada560
Ahmed El ghazawy <sc16ameg@leeds.ac.uk>
2019-03-25 21:31:28 +0000

    Removed the pocketsphinx based script

--- start.py
+++
@@ -1,8 +0,0 @@
-from pocketsphinx impot LiveSpeech
-
-#loop for phrases being said until called for
-for phrase in LiveSpeech():
-    if str(phrase) == "hello robot":
-        print "Hi"
-    else:
-        print phrase
