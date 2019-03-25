bafad71196fcf18f1110abf1735ccca4e99c734d
Ahmed El ghazawy <sc16ameg@leeds.ac.uk>
2019-03-25 21:29:24 +0000

    Created a new script using the SpeechRecognition library.

--- speech.py
+++ speech.py
@@ -1,18 +1,56 @@
 import speech_recognition as sr
+from nltk import pos_tag
+from nltk import RegexpParser
 
-r = sr.Recognizer()
+"""
+Process commands based on the tokens existing in them and their respective types
+"""
+def ProcessCommand(tokens):
+    pass
 
-with sr.Microphone() as source:
-    print('Speak')
-    while True:
-        audio = r.listen(source)
+"""
+Stay on standby and keep listening until the activation word is said.
+When the Activation word is said, listen to the command and process it.
+"""
+def Listen():
+    #Initialise the recogniser to recieve the audio
+    r = sr.Recognizer()
 
-        try:
-            text = r.recognize_google(audio)
-            if text == "hello robot":
-                print("Hello Human!")
-                break
-            else:
-                print('You said : {}'.format(text))
-        except:
-            print('Recognition failed')
+    #Open microphone and listen to what humans say
+    with sr.Microphone() as source:
+
+        #Set robot active status to 0
+        active = 0
+        while True:
+            if active == 0:
+                print('Speak')
+            elif active == 1:
+                print("Speak your command")
+
+            #Listen for command
+            audio = r.listen(source)
+            print("Command recieved")
+
+            try:
+                #Use the google cloud API to turn speech into text
+                text = r.recognize_google(audio)
+                print("Command deciphered")
+                #Activate robot in case keyword was said
+                if text == "hello robot":
+                    print("Hello Human!")
+                    active = 1
+
+                #If robot is active, process the command
+                elif active == 1:
+                    print('You said : {}'.format(text))
+                    text = text.split()
+                    tokens = pos_tag(text)
+                    print(tokens)
+
+                else:
+                    print('You said : {}'.format(text))
+            except:
+                print('Recognition failed')
+
+if __name__ == '__main__':
+    Listen()
