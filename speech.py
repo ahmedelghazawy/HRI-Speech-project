7223e819ae9f04fd47cc465914fdd460131bd623
Ahmed El ghazawy <sc16ameg@leeds.ac.uk>
2019-03-26 21:15:48 +0000

    Activation command set to "hey tiago" Triple handshake performed code minimally refactored

--- speech.py
+++ speech.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 from nltk import pos_tag
 from nltk import RegexpParser
+from nltk import tokenize
 
 #Global variables
 drinks = ["coffee", "tea", "lemonade", "water"]
@@ -10,7 +11,42 @@ objects = ["papers", "stapler", "pen", "book"]
 Process commands based on the tokens existing in them and their respective types
 """
 def ProcessCommand(tokens):
-    pass
+    verb = ""
+    noun = ""
+    #check for verb and noun within the sentence to understand the command
+    for token in tokens:
+        if token[1] == 'VB':
+            verb = token[0]
+        elif token[1] == 'NN':
+            noun = token[0]
+
+    #Check for user confirmation
+    print("would you like me to " + verb + " you " + noun + "?")
+    confirmation = GetCommand()
+
+    #Return to the user the confirmation alongside the items
+    return confirmation, verb, noun
+
+"""
+Get the audio input from user and recognise it
+"""
+def GetCommand():
+    r = sr.Recognizer()
+
+    #Open microphone and listen to what humans say
+    with sr.Microphone() as source:
+
+        #Listen for command
+        audio = r.listen(source)
+
+        try:
+            #Use the google cloud API to turn speech into text
+            command = r.recognize_google(audio)
+            return str(command)
+
+        except:
+            print("Recognition failed")
+            return ""
 
 """
 Stay on standby and keep listening until the activation word is said.
@@ -20,41 +56,46 @@ def Listen():
     #Initialise the recogniser to recieve the audio
     r = sr.Recognizer()
 
-    #Open microphone and listen to what humans say
     with sr.Microphone() as source:
 
         #Set robot active status to 0
-        active = 0
+        active = False
         while True:
             if active == 0:
                 print('Speak')
             elif active == 1:
                 print("Speak your command")
 
-            #Listen for command
-            audio = r.listen(source)
-            print("Command recieved")
+            text = GetCommand()
 
-            try:
-                #Use the google cloud API to turn speech into text
-                text = r.recognize_google(audio)
-                print("Command deciphered")
-                #Activate robot in case keyword was said
-                if text == "hello robot":
-                    print("Hello Human!")
-                    active = 1
+            if text == "stop listening":
+                break
 
-                #If robot is active, process the command
-                elif active == 1:
-                    print('You said : {}'.format(text))
-                    text = text.split()
-                    tokens = pos_tag(text)
-                    print(tokens)
+            #Activate robot in case keyword was said
+            if text == "hey Tiago":
+                print("Hello Human!")
+                active = True
 
-                else:
-                    print('You said : {}'.format(text))
-            except:
-                print('Recognition failed')
+            #If robot is active, process the command
+            elif active == True:
+                print('You said : {}'.format(text))
+                text = str(text).split()
+                tokens = pos_tag(text)
+                #print(tokens)
+                details = ProcessCommand(tokens)
+
+                #Reply to human based on their command
+                if details[0] == "yes":
+                    print("OK, i will " + details[1] + " you " + details[2])
+                    active = False
+                elif details[0] == "no":
+                    continue
+
+            elif text == "":
+                print("Please try again")
+
+            else:
+                print('You said : {}'.format(text))
 
 if __name__ == '__main__':
     Listen()
